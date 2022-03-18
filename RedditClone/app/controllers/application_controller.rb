cafc17181e7c306393a035e302f706da7f3940a2
rmei6 <appacademystudent@Apps-Mac-mini-2.local>
2022-03-18 12:10:39 -0400

    Complete Phase I

--- RedditClone/app/controllers/application_controller.rb
+++ RedditClone/app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+  
+  def login!(user)
+    session[:session_token] = user.session_token
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def log_out
+    current_user.reset_session_token
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def current_user
+    return nil if session[:session_token].nil?
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def require_logged_in
+    redirect_to new_session_url unless logged_in?
+  end
+
+  def require_logged_out
+    redirect_to users_url if logged_in?
+  end
 end
